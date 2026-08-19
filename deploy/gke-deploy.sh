ac515170b2c233af5b9c92643112f8286a4277cb
Ben Sigelman <bhs@gmail.com>
2026-08-18 22:18:29 -0700

    make deploys more reliable

--- deploy/gke-deploy.sh
+++ deploy/gke-deploy.sh
@@ -11,7 +11,8 @@ set -e
 : "${GCP_PROJECT:?Set GCP_PROJECT to your GCP project ID}"
 
 REGION=${REGION:-us-central1}
-IMAGE="$REGION-docker.pkg.dev/$GCP_PROJECT/mendel/mendel:$(git rev-parse --short HEAD)"
+TAG=$(git rev-parse --short HEAD)
+IMAGE="$REGION-docker.pkg.dev/$GCP_PROJECT/mendel/mendel:$TAG"
 
 echo "=== Building $IMAGE ==="
 docker build --platform linux/amd64 -t "$IMAGE" .
@@ -19,7 +20,7 @@ docker build --platform linux/amd64 -t "$IMAGE" .
 echo "=== Pushing ==="
 docker push "$IMAGE"
 
-echo "=== Updating deployment ==="
+echo "=== Updating deployment to $TAG ==="
 kubectl set image deployment/mendel -n mendel mendel="$IMAGE"
 
 echo "=== Waiting for rollout ==="
@@ -29,3 +30,4 @@ echo "=== Running migrations ==="
 kubectl exec -n mendel deployment/mendel -- mendel migrate
 
 echo "=== Done ==="
+echo "Deployed: $IMAGE"
